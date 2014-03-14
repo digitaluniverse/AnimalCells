6eaacb3efb09588a1c50be28a2d4c652572c037e
mateo <mateo@server.digitaluniver.se>
2014-03-13 19:40:09 -0700

    Initial Commit

---
+++ AnimalCells/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  AnimalCells
+//
+//  Created by mateo on 3/13/14.
+//  Copyright (c) 2014 digitaluniverse. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AnimalCells/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  AnimalCells
+//
+//  Created by mateo on 3/13/14.
+//  Copyright (c) 2014 digitaluniverse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
