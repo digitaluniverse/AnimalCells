6b5b1c8152b3e019346da31505dd7ba7c02796cb
mateo <mateo@server.digitaluniver.se>
2014-05-24 18:53:11 -0700

    Animal Cells
    Animal Cells Study and Quiz

--- AnimalCells/FirstViewController.h
+++ AnimalCells/FirstViewController.h
@@ -8,6 +8,59 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface FirstViewController : UIViewController{
+    
+    IBOutlet UIImageView *animalcell;
+    IBOutlet UIButton *vacule;
+    IBOutlet UIButton *ribosomes;
+    IBOutlet UIButton *rougher;
+    IBOutlet UIButton *motochondrion;
+    IBOutlet UIButton *smoother;
+    IBOutlet UIButton *cellmembrane;
+    IBOutlet UIButton *lysosome;
+    IBOutlet UIButton *nuclearmembrane;
+    IBOutlet UIButton *nucleus;
+    IBOutlet UIButton *nucleous;
+    IBOutlet UIButton *cytoplasm;
+    IBOutlet UIButton *golgiapparatus;
+    IBOutlet UIButton *golgivesicle;
+    IBOutlet UIButton *centrioles;
+    IBOutlet UIButton *hide;
+    
+    
+    IBOutlet UITextView *defvacule;
+    IBOutlet UITextView *defribosomes;
+    IBOutlet UITextView *defrougher;
+    IBOutlet UITextView *defmotochondrion;
+    IBOutlet UITextView *defsmoother;
+    IBOutlet UITextView *defcellmembrane;
+    IBOutlet UITextView *deflysosome;
+    IBOutlet UITextView *defnuclearmembrane;
+    IBOutlet UITextView *defnucleus;
+    IBOutlet UITextView *defnucleous;
+    IBOutlet UITextView *defcytoplasm;
+    IBOutlet UITextView *defgolgiapparatus;
+    IBOutlet UITextView *defgolgivesicle;
+    IBOutlet UITextView *defcentrioles;
+    
+}
+
+- (IBAction)hide:(id) sender;
+- (IBAction)vacule:(id)sender;
+- (IBAction)ribosome:(id)sender;
+- (IBAction)rougher:(id)sender;
+- (IBAction)mitochondrion:(id)sender;
+- (IBAction)smoother:(id)sender;
+- (IBAction)cellmembrane:(id)sender;
+- (IBAction)lysosome:(id)sender;
+- (IBAction)nuclearmembrane:(id)sender;
+- (IBAction)nucleus:(id)sender;
+- (IBAction)nucleous:(id)sender;
+- (IBAction)cytoplasm:(id)sender;
+- (IBAction)golgiapparatus:(id)sender;
+- (IBAction)golgivesicle:(id)sender;
+- (IBAction)centrioles:(id)sender;
+
+
 
 @end
--- AnimalCells/FirstViewController.m
+++ AnimalCells/FirstViewController.m
@@ -14,6 +14,261 @@
 
 @implementation FirstViewController
 
+- (IBAction)bootup:(id)sender{
+    
+    
+}
+
+- (IBAction)hide:(id)sender{
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+
+
+- (IBAction)vacule:(id)sender{
+    defvacule.hidden = NO;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)ribosome:(id)sender{
+    defribosomes.hidden = NO;
+    defvacule.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)rougher:(id)sender{
+    defrougher.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)mitochondrion:(id)sender{
+    defmotochondrion.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)smoother:(id)sender{
+    defsmoother.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)cellmembrane:(id)sender{
+    defcellmembrane.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)lysosome:(id)sender{
+    deflysosome.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)nuclearmembrane:(id)sender{
+    defnuclearmembrane.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)nucleus:(id)sender{
+    defnucleus.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)nucleous:(id)sender{
+    defnucleous.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)cytoplasm:(id)sender{
+    defcytoplasm.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)golgiapparatus:(id)sender{
+    defgolgiapparatus.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgivesicle.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)golgivesicle:(id)sender{
+    defgolgivesicle.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defcentrioles.hidden = YES;
+}
+- (IBAction)centrioles:(id)sender{
+    defcentrioles.hidden = NO;
+    defvacule.hidden = YES;
+    defribosomes.hidden = YES;
+    defrougher.hidden = YES;
+    defmotochondrion.hidden = YES;
+    defsmoother.hidden =YES;
+    defcellmembrane.hidden = YES;
+    deflysosome.hidden = YES;
+    defnuclearmembrane.hidden = YES;
+    defnucleus.hidden = YES;
+    defnucleous.hidden = YES;
+    defcytoplasm.hidden = YES;
+    defgolgiapparatus.hidden = YES;
+    defgolgivesicle.hidden = YES;
+}
+
+
+
+
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -27,3 +282,6 @@
 }
 
 @end
+
+
+
